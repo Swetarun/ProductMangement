0723da3470a36f853e6dc5575df773239c792e26
Swetarun <s.singh_279@rediffmail.com>
2022-06-05 20:25:20 +0530

    cart

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const route = require('./route/route')
+const multer= require("multer");
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use( multer().any())
+
+mongoose.connect("mongodb+srv://Swetarun:lBf6gTedHw2tfPtQ@cluster0.ebg8a.mongodb.net/shoppingCart", {
+    useNewUrlParser: true
+})
+.then( () => console.log("MongoDb is connected"))
+.catch( err => console.log(err))
+
+app.use('/', route)
+
+app.all('*', function(req,res){
+    throw new Error("Bad Request")
+})
+
+app.use(function(e,req,res,next){
+    if(e.message=="Bad Request")
+    return res.status(400).send({error : e.message})
+})
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
